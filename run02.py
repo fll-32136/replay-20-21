93097d7709a311fdeb2a597acf1e8f954c857841
Chandana Gottimukkala <chandanagottimukkala@Chandanas-MacBook-Pro.local>
2020-11-11 19:09:17 -0500

    Scrimmage modifications

--- run02.py
+++ run02.py
@@ -9,20 +9,23 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from common import *
 import time
-#Alignment: Right wheel at 2nd horizontal line, front of robot is at 2nd to last vertical line
+#Alignment: Right wheel at 3rd horizontal line, front of robot is at 2nd to last vertical line
 def stepcounter():
     straight(175, 3000)
     time.sleep(1)
     straight(25, 8300)
+    #after stepcounter
     straight(-175, 825)
     pivot_turn(50, 1400)
     pivot_turn(50, 500)
     straight(-175, 1300)
     straight(175, 2800)
+    #after pull-up bar
     turn(50, 800)
     straight(175, 1850)
     while True:
         straight(175, 250)
-        straight(-175, 250)
+        straight(-175, 225)
+    #after robot dance
 
 stepcounter()
