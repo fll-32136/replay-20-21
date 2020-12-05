d871fa66ec3a774f82a2e9b988657fd9bcc4c122
Vihaan <vkerekatte@gmail.com>
2020-12-05 18:02:03 -0500

    Updated run03

--- run03.py
+++ run03.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env pybricks-micropython
+from ucollections import namedtuple
+import urandom
 from pybricks.hubs import *
 from math import *
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
@@ -15,48 +17,45 @@ from time import *
 
 # Create your objects here.
 ev3 = EV3Brick()
-gyro_sensor = GyroSensor(Port.S2)
+gyro_sensor = GyroSensor(Port.S4)
 
 # Alignment: Right wheel on 6th line with robot on back wall
 # Attachment start position 2 cubes at the top
 
 def gyro_finder():
-    gyro_sensor = GyroSensor(Port.S2)
+    gyro_sensor = GyroSensor(Port.S4)
     print(gyro_sensor.angle())
 
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.A)
 
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=46, axle_track=114)
 
 def gyro_stop(gyro_num):   
-    gyro_sensor = GyroSensor(Port.S2)
+    gyro_sensor = GyroSensor(Port.S4)
     gyro_sensor.reset_angle(0)
     while gyro_sensor.angle() != gyro_num:
-        if gyro_sensor.angle() > gyro_num:
-            robot.drive(50,90)
-        elif:
-            robot.drive(50,-90)
-        else:
-    gyro_finder()
+        if gyro_sensor.angle() < gyro_num:
+            robot.drive(50,20)
+        elif gyro_sensor.angle() > gyro_num:
+            robot.drive(50,-20)
 
 def run3():
-    gyro_sensor.reset_angle(0)
     straight(200, 1810)
     gyro_stop(0)
-    gyro_stop(-85)
-    straight(-200, 60)
-    attachment(-95, 580)
-    straight(200, 200)
-    wait(400)
-    attachment(-95, 580)
-    wait(400)
-    straight(200, 250)
-    wait(400)
-    attachment(-95, 580)
-    wait(400)
+    turn(90, 664)
+    straight(200, 30)
+    attachment(-95, 550)
     straight(200, 250)
     wait(400)
     attachment(-95, 600)
+    wait(400)
+    straight(200, 545)
+    wait(400)
+    attachment(-95, 590)
+    wait(400)
+    straight(200, 290)
+    wait(400)
+    attachment(-95, 610)
 
 run3()
