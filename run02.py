5ab631c71af7e468dd2166a461a961d1ea1798ee
Chandana Gottimukkala <chandanagottimukkala@Chandanas-MacBook-Pro.local>
2020-12-20 19:28:47 -0500

    Made run02 changes, and added left and right attachment functions.

--- run02.py
+++ run02.py
@@ -9,19 +9,26 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from common import *
 import time
-#Alignment: Right wheel at 3rd horizontal line, front of robot is at 6th to last vertical line
+#Alignment: Right DRIVE wheel at 3rd horizontal line, front of robot is at 7th to last vertical line(bold line)
 #  remember to tilt a tad bit to the left
 def run02():
     straight(175, 3500)
     time.sleep(1)
-    straight(25, 8450)
+    straight(25, 8160)
     #after stepcounter
     straight(-225, 800)
     pivot_turn(50, 1400)
     pivot_turn(50, 450)
     straight(-175, 1300)
-    straight(175, 800)
-    '''
+    left_att(2000, -200)
+    straight(175, 880)
+    left_att(2000, 200)
+
+'''
+    #do the lifty thing later
+    straight(-175, 650)
+    turn(-50, 1350)
+
     #after pull-up bar
     turn(50, 800)
     straight(175, 1850)
