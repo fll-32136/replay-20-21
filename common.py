70f2074c6fcb255b720c8206057d734752a4f06f
Raghu Vemuri <Raghuram.Vemuri@fmr.com>
2021-01-09 15:47:54 -0500

    Common Changes - Rishi

--- common.py
+++ common.py
@@ -9,12 +9,19 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 import math
+
+straight_wheel_diameter = 56
+turn_wheel_diameter = 46
+axle_track = 114
+motor_a = Motor(Port.A)
+motor_b = Motor(Port.B)
+motor_c = Motor(Port.C)
+motor_d = Motor(Port.D)
+robot = DriveBase(motor_a, motor_b, straight_wheel_diameter, axle_track)
+robot_turn = DriveBase(motor_a, motor_b, turn_wheel_diameter, axle_track)
+robot_att = DriveBase(motor_c, motor_d, wheel_diameter, axle_track)
+
 def straight(Speed, Time):
-    motor = Motor(Port.A)
-    motor1 = Motor(Port.B)
-    wheel_diameter = 46
-    axle_track = 114
-    robot = DriveBase(motor, motor1, wheel_diameter, axle_track)
     robot.drive_time(Speed, 0, Time) #Time in seconds by thousands
     robot.stop(Stop.BRAKE)
 
@@ -32,76 +39,41 @@ def gyrorun(Speed, Time):
             turn(500, y) 
 
 def pivot_turn(direction, degrees):
-    left_motor = Motor(Port.A)
-    right_motor = Motor(Port.B)
-    wheel_diameter = 56
-    axle_track = 114
-    robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
-    robot.drive_time(math.pi * axle_track / 4, direction, degrees)
-    robot.stop(Stop.BRAKE)
+    robot_turn.drive_time(math.pi * axle_track / 4, direction, degrees)
+    robot_turn.stop(Stop.BRAKE)
 
 def turn(direction, degrees):
-    motor = Motor(Port.A)
-    motor1 = Motor(Port.B)
-    wheel_diameter = 46
-    axle_track = 114
-    robot = DriveBase(motor, motor1, wheel_diameter, axle_track)
-    robot.drive_time(0, direction, degrees)
-    robot.stop(Stop.BRAKE)
+    robot_turn.drive_time(0, direction, degrees)
+    robot_turn.stop(Stop.BRAKE)
 
 def turn_forever(direction):
     while True:
         pivot_turn(50,100)
 
 def turn_forever(direction):
-    motor = Motor(Port.A)
-    motor1 = Motor(Port.B)
-    wheel_diameter = 46
-    axle_track = 114
-    robot = DriveBase(motor, motor1, wheel_diameter, axle_track)
-    robot.drive_time(0, direction, 0)
+    robot_turn.drive_time(0, direction, 0)
 
 def left_att(Time, Speed):
     #Negative speed goes up (for lift att)
-    robot = Motor(Port.C, positive_direction=Direction.CLOCKWISE, gears=None)
-    robot.run_time(Speed, Time, then=Stop.HOLD, wait=True)
+    left_att_motor = Motor(Port.C, positive_direction=Direction.CLOCKWISE, gears=None)
+    left_att_motor.run_time(Speed, Time, then=Stop.HOLD, wait=True)
 
 def right_att(Time, Speed): 
-    robot = Motor(Port.D, positive_direction=Direction.CLOCKWISE, gears=None)
-    robot.run_time(Speed, Time, then=Stop.HOLD, wait=True)
+    right_att_motor = Motor(Port.D, positive_direction=Direction.CLOCKWISE, gears=None)
+    right_att_motor.run_time(Speed, Time, then=Stop.HOLD, wait=True)
 
 def strforever(Speed):
-    motor = Motor(Port.A)
-    motor1 = Motor(Port.B) 
-    wheel_diameter = 56
-    axle_track = 114
-    robot = DriveBase(motor, motor1, wheel_diameter, axle_track)
     robot.drive(Speed, 0)
 
 def strstop():
-    motor = Motor(Port.A)
-    motor1 = Motor(Port.B) 
-    wheel_diameter = 56
-    axle_track = 114
-    robot = DriveBase(motor, motor1, wheel_diameter, axle_track)
     robot.stop()
 
-def straight_(Speed, Time):
-    motor = Motor(Port.C)
-    motor1 = Motor(Port.D)
-    wheel_diameter = 56
-    axle_track = 114
-    robot = DriveBase(motor, motor1, wheel_diameter, axle_track)
-    robot.drive_time(Speed, 0, Time) #Time in seconds by thousands
-    robot.stop(Stop.BRAKE)
+def straight_att(Speed, Time):
+    robot_att.drive_time(Speed, 0, Time) #Time in seconds by thousands
+    robot_att.stop(Stop.BRAKE)
 
 # Use negative direction for right motor
 def attachment(direction, degrees):
-    motor = Motor(Port.C)
-    motor1 = Motor(Port.D)
-    wheel_diameter = 56
-    axle_track = 114
-    robot = DriveBase(motor, motor1, wheel_diameter, axle_track)
     robot.drive_time(math.pi * axle_track / 4, direction, degrees) #Time in seconds by thousands
     robot.stop(Stop.BRAKE)
 
@@ -114,17 +86,14 @@ def color_finder():
     line_sensor = ColorSensor(Port.S3)
     print(line_sensor.reflection())
 
-def color_stop(color_num):   
-    line_sensor = ColorSensor(Port.S3)
-    while line_sensor.reflection() != color_num:  #when color is not black
-        straight(200,200) #go straight
+
 
 def colorstop(colorsensor):
     while True:
         if colorsensor.reflection()<=9:
-            straight_robot.stop()
+            robot.stop()
             break
-    straight_robot.stop()
+    robot.stop()
 
 def infiniteturn():
     while True:
