80b7f97902e00a5d08333cfe749b410af1a648d2
ipranav0809 <ipranav0809@gmail.com>
2021-01-15 18:18:40 -0500

    yahooo

--- run02.py
+++ run02.py
@@ -24,55 +24,63 @@ rightcolor = ColorSensor(Port.S1)
 #Alignment: Right DRIVE wheel at 3rd horizontal line, front of robot is at 7th to last vertical line(bold line)
 #  remember to tilt a tad bit to the left
 def run02():
-    # # launch to stepcounter
-    # straight(175, 3300)
-    # # time.sleep(1)
-    # # does stepcounter slowly
-    # straight(20, 10500)  
-    # # time.sleep(1)
-    # #backs up from stepcounter and turns
-    # straight(-200, 400)
-    # straight_robot.drive(-100, 0)
-    # colorstop(straight_robot, rightcolor, 9)
-    # straight(200, 265)
-    # # straight(-150, 800)
-    # pivot_turn(50, 1200)
-    # pivot_turn(50, 450)
-    # # # back up to the wall
-    # straight(-175, 1250)
-    # # # launch to pull up bar and drop health units
-    # left_att(2100, -200)
-    # # time.sleep(1)
-    # straight(100, 1550)
-    # # time.sleep(2)
-    # left_att(2300, 200)
-    # # launch to treadmill and does treadmill
-    # straight(-75, 1400)
-    # # remember this is the oldone pivot_turn(-50, 1580)
-    # turn(-85, 780)
-    # straight(170, 3550)
-    # right_att(8000, 2000)
-    # # after treadmill
-    # straight(-250, 300)
-    # pivot_turn(50, 1500)
-    # straight(-250, 1500)
-    # # aligns by backing up to the wall
-    # straight(250, 100)
-    # pivot_turn(50, 350)
-    # straight(250, 400)
-    # left_att(1200, -200)
-    # pivot_turn(-50, 1670)
-    # straight( 250, 230)
-    # left_att(1200, 200)
-    # straight(-100, 1300)
-    # pivot_turn(50, 700)
-    # # finishes row machine
-    # left_att(2000, -200)
+    # launch to stepcounter
+    straight(175, 3300)
+    # time.sleep(1)
+    # does stepcounter slowly
+    straight(20, 10500)  
+    # time.sleep(1)
+    #backs up from stepcounter and turns
     straight(-200, 400)
-    turn(85, 600)
-    line_square(9,5, 120)
-    turn(85, 800)
-    straight(300, 9000)
+    straight_robot.drive(-100, 0)
+    colorstop(straight_robot, rightcolor, 9)
+    straight(200, 265)
+    # straight(-150, 800)
+    pivot_turn(50, 1200)
+    pivot_turn(50, 450)
+    # # back up to the wall
+    straight(-175, 1250)
+    # # launch to pull up bar and drop health units
+    left_att(2100, -200)
+    # time.sleep(1)
+    straight(100, 1550)
+    # time.sleep(2)
+    left_att(2300, 200)
+    # launch to treadmill and does treadmill
+    straight(-75, 1400)
+    # remember this is the oldone pivot_turn(-50, 1580)
+    turn(-85, 740) # 780 #740
+    straight(170, 3550)
+    right_att(8000, 2000)
+    # after treadmill
+    straight(-250, 300)
+    pivot_turn(50, 1500)
+    straight(-250, 1500)
+    # aligns by backing up to the wall
+    straight(250, 100)
+    pivot_turn(50, 350)
+    straight(250, 400)
+    left_att(1200, -200)
+    pivot_turn(-50, 1670)
+    straight( 250, 200)
+    left_att(1200, 200)
+    straight(-100, 1150)
+    pivot_turn(50, 400)
+    # finishes row machine
+    
+    # # left_att(2000, -200)
+    # # straight(-200, 300)
+    # # turn(85, 500)
+    # # line_square(9,5, 120)
+    
+    # # straight(200, 450)
+    
+    # # turn(85, 855)
+    # # straight(5000, 7000)
+    left_att(2000, -200)
+    straight(-200, 800)
+    turn(-85, 200)
+    straight(-5000, 6000)
     # straight(200, 1200)
     # turn(-85, 810)
     # straight(200, 300)
