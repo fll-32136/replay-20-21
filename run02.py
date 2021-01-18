6fd794671ffd87b7f1e2de8d337596ddb452f1d7
ipranav0809 <ipranav0809@gmail.com>
2021-01-18 13:54:20 -0500

    Run 2 1/18/21 changes

--- run02.py
+++ run02.py
@@ -35,23 +35,23 @@ def run02():
     straight(-200, 400)
     straight_robot.drive(-100, 0)
     colorstop(straight_robot, rightcolor, 9)
-    straight(200, 280)
+    straight(200, 310)
     # straight(-150, 800)
     pivot_turn(50, 1200)
     pivot_turn(50, 450)
     # # back up to the wall
     straight(-175, 1250)
     # # launch to pull up bar and drop health units
-    left_att(2100, -200)
+    left_att(2000, -300)
     # time.sleep(1)
     straight(100, 1550)
     # time.sleep(2)
-    left_att(2300, 200)
+    left_att(2000, 300)
     # launch to treadmill and does treadmill
     straight(-75, 1400)
     # remember this is the oldone pivot_turn(-50, 1580)
     turn(-85, 810) # 780 #740
-    straight(170, 3550)
+    straight(170, 3600)
     right_att(8000, 2000)
     # after treadmill
     straight(-250, 300)
@@ -61,19 +61,18 @@ def run02():
     straight(250, 100)
     pivot_turn(50, 350)
     straight(250, 400)
-    left_att(1200, -200)
+    left_att(1200, -300)
     pivot_turn(-50, 1670)
     straight( 250, 250)
-    left_att(1200, 200)
+    left_att(1200, 300)
+    # Hooks onto row machine
     straight(-100, 1100)
     pivot_turn(50, 600)
     # finishes row machine
-    left_att(2000, -200)
+    left_att(1700, -300)
     straight(-200, 800)
     turn(-85, 200)
     straight(-5000, 7000)
-    
-
     # launches to row machine
 # run02()
     
